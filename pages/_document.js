f1e07291b92ab6aebfc7667cae78395dcf5c9833
David Pett <me@davidpett.com>
2018-11-01 16:16:45 -0500

    revert to default style collection

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,6 @@
 import Document, { Head, Main, NextScript } from 'next/document'
 import React from 'react'
 import { AppRegistry } from 'react-native-web'
-import { ServerStyleSheet } from 'styled-components'
 
 // Force Next-generated DOM elements to fill their parent's height
 const normalizeNextElements = `
@@ -15,12 +14,10 @@ const normalizeNextElements = `
 export default class MyDocument extends Document {
   static async getInitialProps({ renderPage }) {
     AppRegistry.registerComponent('Main', () => Main)
-
-    const sheet = new ServerStyleSheet()
-    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
-    const styles = sheet.getStyleElement()
-
-    return { ...page, styles }
+    const { getStyleElement } = AppRegistry.getApplication('Main')
+    const page = renderPage()
+    const styles = [<style dangerouslySetInnerHTML={{ __html: normalizeNextElements }} />, getStyleElement()]
+    return { ...page, styles: React.Children.toArray(styles) }
   }
 
   render() {
@@ -28,7 +25,6 @@ export default class MyDocument extends Document {
       <html style={{ height: '100%' }}>
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          {this.props.styles}
         </Head>
         <body style={{ height: '100%', overflow: 'hidden' }}>
           <Main />
